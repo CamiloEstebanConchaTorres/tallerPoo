e966e10394ec4bf27fb26a18b20220342d71801e
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com.>
2024-08-05 19:52:48 -0500

    feat: :construction: Primer ejercicio de la clase persona realizado
    cree una clase persona que tuviera atributos como nombre edad y sexo y cree un metodo saludar para imprimir el nombre

---
+++ persona.js
@@ -0,0 +1,35 @@
+// 1. Crea una clase llamada Persona que tenga las siguientes
+// propiedades:nombre,edad, sexoyunmétodollamadosaludar
+// que imprima en la consola un saludo con el nombre de la persona.
+
+class persona {
+    nombre
+    edad
+    sexo
+    constructor(nombre, edad, sexo){
+        this.nombre = nombre
+        this.edad = edad
+        this.sexo = sexo
+    }
+    set setnombre(nombre){
+        this.nombre = nombre
+    }
+    get getnombre(){
+        return this.nombre
+    }
+    set setedad(edad){
+        this.edad = edad
+    }
+    get getedad(){
+        return this.edad
+    }
+    set setsexo(sexo){
+        this.sexo = sexo
+    }
+    get getsexo(){
+        return this.sexo
+    }
+    saludar(){
+        return `Hola mi nombre es ${this.nombre}`
+    }
+}
